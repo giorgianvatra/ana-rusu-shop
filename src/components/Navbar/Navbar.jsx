546fde408425a6de9a2f20490e42f52a93231cd1
Giorgian Vatra <vatragiorgian@gmail.com>
2023-12-26 22:50:06 +0200

    Settup start

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus veniam aliquam magnam natus architecto facilis, velit quas accusamus voluptate maxime amet autem? Quis debitis aliquid, eum officia unde dolorem labore!</p>
+    </div>
+    
+  )
+}
+
+export default Navbar
