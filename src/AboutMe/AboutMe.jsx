f219076ea72b59b3f0109dadd158885e8882a31c
Giorgian Vatra <vatragiorgian@gmail.com>
2024-07-07 23:43:25 +0300

    about me section done

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -1,18 +1,33 @@
+import ProfileIntroductionAndPhoto from "./ProfileIntroductionAndPhoto/ProfileIntroductionAndPhoto";
 
 function AboutMe() {
   return (
-    <>
-      <div className="header-container">
-        <div className="intro">
-          <h1>Hi!</h1>
-          <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum iusto iure, dolorem dicta veniam soluta cum officia facilis maiores.</h5>
-        </div>
-        <div className="image ">
-
-        </div>
+    <div className="flex flex-col items-center ">
+      <ProfileIntroductionAndPhoto />
+      <div className="text-about-me-area shadow-2xl py-[2rem] w-[90%] rounded-lg bg-white mt-[2rem]">
+        <p className="mt-[1rem] pl-5">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem, nulla! Minus eaque veniam expedita rerum vero porro
+          excepturi nemo libero ducimus incidunt ullam, enim quibusdam voluptate
+          animi. Nostrum aperiam quo minus inventore veniam facilis illo quod
+          dolorum. Nobis repellat exercitationem modi laboriosam! Lorem ipsum
+          
+        </p>
+        <p className="mt-[1rem] pl-5">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Exercitationem, nulla! Minus eaque veniam expedita rerum vero porro
+          excepturi nemo libero ducimus incidunt ullam, enim quibusdam voluptate
+          animi. Nostrum aperiam quo minus inventore veniam facilis illo quod
+          dolorum. Nobis repellat exercitationem modi laboriosam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Rerum, voluptate
+          laudantium recusandae harum necessitatibus accusamus officia debitis
+          deleniti ut laborum pariatur, numquam dolore molestias quisquam a
+          praesentium doloribus? Hic quaerat cum animi soluta, ad eveniet
+    
+        </p>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
---
+++ src/AboutMe/ProfileIntroductionAndPhoto/ProfileIntroductionAndPhoto.jsx
@@ -0,0 +1,28 @@
+import ProfilePicture from "../img/profile-picture.jpg";
+
+function ProfileIntroductionAndPhoto() {
+  return (
+    <>
+      <div className="header-container flex justify-center items-center mt-[1rem] shadow-2xl py-[2rem] w-[90%] rounded-lg">
+        <div className="intro w-[40vw]">
+          <h1>Hi!</h1>
+          <h5>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus rerum iusto iure, dolorem dicta veniam soluta cum
+            officia facilis maiores.
+          </h5>
+        </div>
+        <div className="image ">
+          <img
+            src={ProfilePicture}
+            className=" h-[20rem] object-cover"
+            alt=""
+          />
+        </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default ProfileIntroductionAndPhoto;
